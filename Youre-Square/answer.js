68ba58c07cc8e1ad226a5b4b9b6dfaa21aa86a46
ramialkaro <rami.alkaro@gmail.com>
2021-04-30 00:45:13 +0300

    You're a square

---
+++ Youre-Square/answer.js
@@ -0,0 +1,12 @@
+
+// Read the README.md file first to understand the Question or problem
+
+// 👍
+
+// 
+var isSquare = function(n){
+  if(n >= 0){
+    return Math.sqrt(n) % 1 === 0 && true 
+  }
+  return false; 
+}
